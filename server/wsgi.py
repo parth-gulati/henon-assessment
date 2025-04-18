791ee310399f4bc1190d5159c5e55cbfe66a9302
Parth Gulati <gulati.parth98@gmail.com>
2025-04-18 20:02:26 +0530

    v1.0.10: Moved Procfile out to root dir for deployment purposes

--- server/wsgi.py
+++ server/wsgi.py
@@ -1,4 +1,6 @@
+import os
 from app import app
 
 if __name__ == "__main__":
-    app.run()
+    port = int(os.environ.get("PORT", 8000))  # Default to 8000 if PORT is not set
+    app.run(host="0.0.0.0", port=port, debug=True)
