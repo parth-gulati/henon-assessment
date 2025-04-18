76f8c5964ef7ca126db964587d166d2621bcced4
Parth Gulati <gulati.parth98@gmail.com>
2025-04-18 20:32:01 +0530

    v1.0.15: Downgraded date-fns version to fix deployment issues

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFnsv3';
+import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
 
 import './index.css'
 import App from './App.jsx'
